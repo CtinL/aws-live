b1dbe7a61ca3128c962af3e81a5a43d56608ca19
CtinL <109510285+CtinL@users.noreply.github.com>
2022-09-26 17:09:36 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "cl-employee.cobcscllth9c.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
-customdb = "cl-employee"
+customdb = "clemployee"
 custombucket = "cl-employee"
 customregion = "us-east-1"
